a633f8ae1b7f56e416a9964183dea29420dc4e7f
Ramya Vardhan <ramyavardhan@freshdesk.com>
2015-07-11 20:03:48 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MadrastersForum::Application.config.secret_key_base = '6337f351f0b9896766c83168f163ec2d9e11c3d8551f7779bcffa3479f519438da3dd9d3be8c7f09d3d0c299348ec733314136dadaf3fa3f9c65cd93e8cccda8'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+MadrastersForum::Application.routes.draw do
+  devise_for :users
+  resources :posts
+  root 'posts#index'
+end
