3448a345de349d9f2a1770fa25ceb9a9958538a7
Ramya Vardhan <ramyavardhan@freshdesk.com>
2015-11-10 01:06:45 +0530

    Location - Controller and Views added

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,45 @@
+class LocationsController < ApplicationController
+    before_action :load_location, :only => [:show,:edit,:update,:destroy]  
+
+    def index
+    	@locations = Location.all.order("name")
+    end
+
+    def new
+    	@location =  Location.new()
+    end
+
+    def create
+    	@location = current_user.build_location(post_params)
+
+    	if @location.save
+			redirect_to @location
+		else
+			render 'new'
+		end
+    end
+
+	def update
+		if @location.update(post_params)
+			redirect_to @location
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@location.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def load_location
+		@location = Location.find(params[:id])
+	end
+
+	def post_params
+		params.require(:location).permit(:name,:manager_id)
+	end
+
+end
