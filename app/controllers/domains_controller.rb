4d6574d6e0b025d00cae15d6fe312a154f31a02e
Ramya Vardhan <ramyavardhan@freshdesk.com>
2015-11-10 01:19:09 +0530

    Domain - Controller and Views added

---
+++ app/controllers/domains_controller.rb
@@ -0,0 +1,45 @@
+class DomainsController < ApplicationController
+	  before_action :load_domain, :only => [:show,:edit,:update,:destroy]  
+
+	def index
+		@domains = Domain.all.order("name")
+	end
+
+	def new
+		@domain =  Domain.new()
+	end
+
+	def create
+    	@domain = current_user.domains.build(post_params)
+
+    	if @domain.save
+			redirect_to @domain
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		if @domain.update(post_params)
+			redirect_to @domain
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@domain.destroy
+		redirect_to root_path
+	end
+
+	private
+	
+	def load_domain
+		@domain = Domain.find(params[:id])
+	end
+
+	def post_params
+		params.require(:domain).permit(:name,:lead_id)
+	end
+
+end
